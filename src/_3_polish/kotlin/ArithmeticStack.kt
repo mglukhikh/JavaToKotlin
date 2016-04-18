eb2996ff3c0f329d33b25ec32ab1a840b9dde06b
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2016-04-18 12:44:10 +0300

    _3_polish: enum refactored a little bit

--- src/_3_polish/kotlin/ArithmeticStack.kt
+++ src/_3_polish/kotlin/ArithmeticStack.kt
@@ -8,11 +8,19 @@ class ArithmeticStack {
     private val stack = LinkedList<Double>()
 
     // Enums and lambdas
-    enum class Operation(internal val function: (Double, Double) -> Double) {
-        PLUS({ x, y -> x + y }),
-        MINUS({ x, y -> y - x }),
-        TIMES({ x, y -> x * y }),
-        DIV({ x, y -> y / x })
+    enum class Operation {
+        PLUS,
+        MINUS,
+        TIMES,
+        DIV;
+
+        val function: (Double, Double) -> Double
+            get() = when (this) {
+                PLUS ->  { x, y -> x + y }
+                MINUS -> { x, y -> y - x }
+                TIMES -> { x, y -> x * y }
+                DIV ->   { x, y -> y / x }
+            }
     }
 
     // Expression function syntax
