8ca21a0b8d84af03a7e537199afd5e122b0f250a
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2016-04-21 15:32:27 +0300

    _1_hello synchronized with the presentation

--- src/_1_hello/java/Main.java
+++ src/_1_hello/java/Main.java
@@ -3,6 +3,6 @@ package _1_hello.java;
 public class Main {
     public static void main(String[] args) {
         String name = args.length > 0 ? args[0] : "Java";
-        System.out.println("Hello, " + name);
+        System.out.println("Hello, " + name + ", I am glad to see you");
     }
 }
--- src/_1_hello/kotlin/Main.kt
+++ src/_1_hello/kotlin/Main.kt
@@ -1,8 +1,10 @@
 package _1_hello.kotlin
 
+fun Array<*>.firstOrNull() = if (this.size >= 1) this[0] else null
+
 fun main(args: Array<String>) {
     // Extension function + Elvis
     val name = args.firstOrNull() ?: "Kotlin"
     // String template
-    println("Hello, $name")
+    println("Hello, $name, I am glad to see you")
 }
