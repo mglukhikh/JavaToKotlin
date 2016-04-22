e0bd22a9375d2ff28bb13ed6e95d96152635d0c2
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2016-04-22 15:43:34 +0300

    _5_polish: no object Calculator

--- src/_5_polish/kotlin/Calculator.kt
+++ src/_5_polish/kotlin/Calculator.kt
@@ -2,39 +2,36 @@ package _5_polish.kotlin
 
 import java.util.*
 
-// Singleton
-object Calculator {
-    private val operationMap = mapOf<String, (Double, Double) -> Double>(
-            "+" to { x, y -> x + y },
-            "-" to { x, y -> y - x },
-            "*" to { x, y -> x * y },
-            "/" to { x, y -> y / x }
-    )
+private val operationMap = mapOf<String, (Double, Double) -> Double>(
+        "+" to { x, y -> x + y },
+        "-" to { x, y -> y - x },
+        "*" to { x, y -> x * y },
+        "/" to { x, y -> y / x }
+)
 
-    fun Stack<Double>.execute(op: (Double, Double) -> Double) = push(op(pop(), pop()))
+fun Stack<Double>.execute(op: (Double, Double) -> Double) = push(op(pop(), pop()))
 
-    fun calc(expr: String): Double {
-        val stack = Stack<Double>()
-        for (arg in expr.split(" ")) {
-            val op = operationMap[arg]
-            if (op != null) {
-                stack.execute(op)
-            }
-            else {
-                stack.push(arg.toDouble())
-            }
+fun polishCalculate(expr: String): Double {
+    val stack = Stack<Double>()
+    for (arg in expr.split(" ")) {
+        val op = operationMap[arg]
+        if (op != null) {
+            stack.execute(op)
         }
-        return stack.pop()
-    }
-
-    fun calcFunctional(expr: String): Double {
-        val stack = Stack<Double>()
-        // Extension: for-each
-        expr.split(" ").forEach {
-            // it = single argument
-            // let: receiver --> argument                     // elvis, toDouble()
-            operationMap[it]?.let { op -> stack.execute(op) } ?: stack.push( it.toDouble() )
+        else {
+            stack.push(arg.toDouble())
         }
-        return stack.pop()
     }
+    return stack.pop()
+}
+
+fun polishCalculateFunctional(expr: String): Double {
+    val stack = Stack<Double>()
+    // Extension: for-each
+    expr.split(" ").forEach {
+        // it = single argument
+        // let: receiver --> argument                     // elvis, toDouble()
+        operationMap[it]?.let { op -> stack.execute(op) } ?: stack.push( it.toDouble() )
+    }
+    return stack.pop()
 }
--- test/_5_polish/kotlin/CalculatorTest.kt
+++ test/_5_polish/kotlin/CalculatorTest.kt
@@ -8,15 +8,15 @@ class CalculatorTest {
 
     @Test
     fun testCalc() {
-        assertEquals(7.0, Calculator.calc("1 2 3 * +"), 1e-6)
-        assertEquals(10.0, Calculator.calc("6 -4 -"), 1e-6)
-        assertEquals(3.0, Calculator.calc("10 1 2 3 * + -"), 1e-6)
+        assertEquals(7.0, polishCalculate("1 2 3 * +"), 1e-6)
+        assertEquals(10.0, polishCalculate("6 -4 -"), 1e-6)
+        assertEquals(3.0, polishCalculate("10 1 2 3 * + -"), 1e-6)
     }
 
     @Test
     fun testCalcFunctional() {
-        assertEquals(7.0, Calculator.calcFunctional("1 2 3 * +"), 1e-6)
-        assertEquals(10.0, Calculator.calcFunctional("6 -4 -"), 1e-6)
-        assertEquals(3.0, Calculator.calcFunctional("10 1 2 3 * + -"), 1e-6)
+        assertEquals(7.0, polishCalculateFunctional("1 2 3 * +"), 1e-6)
+        assertEquals(10.0, polishCalculateFunctional("6 -4 -"), 1e-6)
+        assertEquals(3.0, polishCalculateFunctional("10 1 2 3 * + -"), 1e-6)
     }
 }
