402557011798fb66a6f8ac73500849ba52262a0c
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2016-04-22 15:27:35 +0300

    kotlin Line: no companion object inside

--- src/_3_geometry/kotlin/Line.kt
+++ src/_3_geometry/kotlin/Line.kt
@@ -3,22 +3,17 @@ package _3_geometry.kotlin
 //import _3_geometry.java.Point;
 
 // ax + by - 1 = 0
-class Line(private val a: Double, private val b: Double) {
+class Line(val a: Double, val b: Double)
 
-    fun getDistance(p: Point): Double {
-        // Property access
-        return Math.abs(a * p.x + b * p.y - 1) / Math.sqrt(a * a + b * b)
-    }
+// ax1 + by1 = 1
+// ax2 + by2 = 1
+fun create(p1: Point, p2: Point): Line {
+    val d = p1.x * p2.y - p1.y * p2.x
+    val da = p2.y - p1.y
+    val db = p1.x - p2.x
+    return Line(da / d, db / d)
+}
 
-    // Object bound to a class
-    companion object {
-        // ax1 + by1 = 1
-        // ax2 + by2 = 1
-        fun create(p1: Point, p2: Point): Line {
-            val d = p1.x * p2.y - p1.y * p2.x
-            val da = p2.y - p1.y
-            val db = p1.x - p2.x
-            return Line(da / d, db / d)
-        }
-    }
+fun Line.distance(p: Point): Double {
+    return Math.abs(a * p.x + b * p.y - 1) / Math.sqrt(a * a + b * b)
 }
--- test/_3_geometry/kotlin/LineTest.kt
+++ test/_3_geometry/kotlin/LineTest.kt
@@ -10,9 +10,9 @@ class LineTest {
     fun testGetDistance() {
         val p1 = Point(0.0, 3.0)
         val p2 = Point(4.0, 0.0)
-        val line = Line.create(p1, p2)
-        assertEquals(0.0, line.getDistance(p1), 1e-6)
-        assertEquals(0.0, line.getDistance(p2), 1e-6)
-        assertEquals(0.0, line.getDistance(Point(2.0, 1.5)), 1e-6)
+        val line = create(p1, p2)
+        assertEquals(0.0, line.distance(p1), 1e-6)
+        assertEquals(0.0, line.distance(p2), 1e-6)
+        assertEquals(0.0, line.distance(Point(2.0, 1.5)), 1e-6)
     }
 }
